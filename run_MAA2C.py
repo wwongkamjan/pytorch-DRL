a3d79cc21408d25d20d21cee0d65bb4e40d29ac5
wwongkamjan <w.wongkamjan@gmail.com>
2022-04-01 17:26:35 -0700

    add on-policy Q to target val

--- run_MAA2C.py
+++ run_MAA2C.py
@@ -13,7 +13,7 @@ from ray.rllib.env.multi_agent_env import MultiAgentEnv, make_multi_agent
 # from ray.rllib.examples.env.multi_agent import MultiAgentCartPole
 
 
-MAX_EPISODES = 500
+MAX_EPISODES = 5000
 EPISODES_BEFORE_TRAIN = 10
 EVAL_EPISODES = 10
 EVAL_INTERVAL = 100
